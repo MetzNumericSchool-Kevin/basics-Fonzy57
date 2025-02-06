d0c087fdc556a9f77a30524cd3a0bac2a4038162
Fonzy57 <traben@hotmail.fr>
2025-02-06 17:59:53 +0100

    Add exercice définitions de variables

---
+++ exercices.js
@@ -0,0 +1,10 @@
+/* ----------------------- */
+/* Définition de variables */
+/* ----------------------- */
+
+const shopName = "L’Herboristerie de Fondcombe";
+const currency = "€";
+let numberOfHealingPotions = 57;
+let priceOfHealingPotion = 2.90;
+let isShopOpen = true;
+
